888da291e45f10d1338bf60f8b2fa66feef467f9
Gabriel Hollis <gabrielhollis.real@gmail.com>
2026-08-01 16:52:21 -0400

    Delete src directory

--- src/processing/aurx.h
+++
@@ -1,6 +0,0 @@
-#ifndef AURX_H
-#define AURX_H
-#include "bytecoder.h"
-void toAurX(byteCode* c, char* toSaveAs);
-void fromAurX(bool isValid, char* toRead);
-#endif 
--- src/winInclude.h
+++
@@ -1,9 +0,0 @@
-#ifndef WININCLUDE
-#define WININCLUDE
-
-#define isDebug false
-#define extension ".aur"
-int getACP();
-void setConsoleOutput(int n);
-void fatalError(int code, char* n, int line);
-#endif
